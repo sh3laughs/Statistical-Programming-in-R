005f5c5422715027226e56c2634f9b556d29f561
sh3laughs <113483383+sh3laughs@users.noreply.github.com>
2022-10-12 22:24:31 -0700

    Lesson 2 practice work
    Note that this is the first lesson in the repo - lesson 1 had no coding

---
+++ DSO102-L2.R
@@ -0,0 +1,33 @@
+#DSO102 - Statistical Programming in R
+  #Lesson 2 - Introduction to R
+
+
+  #Page 6 - R Script Files
+    #Part 1
+#This is to demonstrate comments
+
+#This function will print whatever is in the parenthesis
+
+print("Hey all you experienced Data Scientists!")
+
+print("That was fun. Goodbye!")
+
+# Part I Hands On
+
+# Part II Hands On
+
+#Here is my answer... I'm explaining it.
+
+
+    #Part 2
+print("Hello World!")
+print("Hallo Welt!")
+print("Hola mundo!")
+
+
+    #Part 3
+# The Comment Script
+# Written on October 11, 2017
+# This script does very little except demonstrate comments
+print("Too many comments") # This line actually does something
+# Note to self: add more lines that do something
